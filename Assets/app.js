517e6dc69fb4470c0fe821573c1d4640845c0ac3
Guenez Muhamed <74774448+Kevin-Nemo73@users.noreply.github.com>
2021-04-24 15:10:35 +0100

    Add files via upload

---
+++ Assets/app.js
@@ -0,0 +1,11 @@
+let nobtn = document.getElementById("nobtn");
+let yesbtn = document.getElementById("yesbtn");
+
+nobtn.addEventListener('click',function(){
+    alert(":( Pwease say yes :(");
+});
+
+yesbtn.addEventListener('click',function(){
+    alert("🖤happiest & luckiest🖤");
+    alert("🖤love you baby🖤");
+});
